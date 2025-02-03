8f3fbd30f32b9cb32a2156f958692865fae0d427
Nawid94 <149882041+Nawid94@users.noreply.github.com>
2025-02-03 10:02:30 +0330

    Update TELEGRAM-OL.py

--- TELEGRAM-OL.py
+++ TELEGRAM-OL.py
@@ -1,24 +1,24 @@
 from datetime import datetime
 import requests
 import os
+import pytz
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHANNEL_ID = os.getenv('TELEGRAM_CHAT_ID')  
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+TEHRAN_TZ = pytz.timezone('Asia/Tehran')
 
 def send_time_to_telegram():
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    message = f"Now: {current_time}"
+    current_time = datetime.now(TEHRAN_TZ).strftime("%Y-%m-%d %H:%M:%S")
+    message = f"Now in Tehran: {current_time}"
     params = {
         'chat_id': TELEGRAM_CHANNEL_ID,
         'text': message
     }
     response = requests.post(TELEGRAM_API_URL, params=params)
     if response.status_code == 200:
-        print("ok")
+        print("Message sent successfully!")
     else:
-        print(f"error: {response.status_code}")
-
+        print(f"Error sending message: {response.status_code}")
 
 send_time_to_telegram()
-
